eb0b30ff82c3489e97e1ec7610e22c868d0769a7
Jemima <jemima.ramoscampanha@gmail.com>
2021-02-10 17:31:03 -0300

    Concluido Prova

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {HomeComponent}  from './components/home/home.component'
+import {BlocksComponent} from './pages/blocks/blocks.component';
+import {CardsComponent} from './pages/cards/cards.component';
+import {FormsComponent} from './pages/forms/forms.component';
+import {PricingComponent} from './pages/pricing/pricing.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+},
+{
+  path: 'cards',
+  component: CardsComponent 
+},
+{
+  path: 'blocks',
+  component: BlocksComponent 
+},
+{
+  path: 'forms',
+  component: FormsComponent 
+},
+{
+  path: 'pricing',
+  component: PricingComponent 
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
+import { BlocksComponent } from './pages/blocks/blocks.component';
+import { FormsComponent } from './pages/forms/forms.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    CardsComponent,
+    PricingComponent,
+    BlocksComponent,
+    FormsComponent,
+   
   ],
   imports: [
     BrowserModule,
